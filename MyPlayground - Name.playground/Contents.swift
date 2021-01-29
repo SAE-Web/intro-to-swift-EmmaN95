c0cb4bbb87fa8ca91b6be0b53e4d868c7a91a229
Emma <60002871+EmmaN95@users.noreply.github.com>
2021-01-29 10:46:16 +0000

    Add files via upload

---
+++ MyPlayground - Name.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var firstName = "Emma"
+
+var lastName = "Ndabai"
+
+var fullName: String = firstName + " " + lastName
+
+let dateOfBirth = 1995/05/15
+
+//let displayPerson = (fullName + "is " + dateOfBirth + "years old ");
+//
+//function displayPerson (firstName, lastName, dateOfBirth) {
+//      console.log(fullName, "is", dateOfBirth, "years old ");
+//}
