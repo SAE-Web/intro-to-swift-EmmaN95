d6df30fb8deacf4b364a91cf162f5edaa53ea205
Emma <60002871+EmmaN95@users.noreply.github.com>
2021-01-29 11:01:40 +0000

    Add files via upload

--- MyPlayground - Name.playground/Contents.swift
+++ MyPlayground - Name.playground/Contents.swift
@@ -10,8 +10,4 @@ var fullName: String = firstName + " " + lastName
 
 let dateOfBirth = 1995/05/15
 
-//let displayPerson = (fullName + "is " + dateOfBirth + "years old ");
-//
-//function displayPerson (firstName, lastName, dateOfBirth) {
-//      console.log(fullName, "is", dateOfBirth, "years old ");
-//}
+print(fullName, "is", dateOfBirth, "years old ")
